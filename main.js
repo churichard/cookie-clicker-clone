7417705e4f53c00d89f669c52a1ad94e70c3ef3e
Richard Chu <richardchu10@gmail.com>
2016-04-21 22:41:10 -0400

    Refactor code

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ var score = 0; // score of the game
 var cookies = 0; // number of cookies you have
 var cursors = 0; // number of cursors you have
 var grandmas = 0; // number of grandmas you have
+var cursorCost = 20 + 20 * cursors/2; // How much it costs to buy a cursor
+var grandmaCost = 100 + 100 * grandmas/2; // How much it costs to buy a grandma
+var cursorBtn = document.getElementById("cursorBtn"); // Cursor button element
+var grandmaBtn = document.getElementById("grandmaBtn"); // Grandma button element
+var numOfCookiesElm = document.getElementById("cookiesCount"); // Number of cookies HTML element
+var scoreElm = document.getElementById("score"); // Score HTML element
 
 // On cookie click
 function cookieClick() {
@@ -12,26 +18,24 @@ function cookieClick() {
 
 // On cursor button click
 function cursorClick() {
-    var cursorCost = 20 + 20 * cursors/2;
     if (cookies >= cursorCost) {
         cookies -= cursorCost;
         cursors++;
 
         cursorCost = 20 + 20 * cursors/2;
-        document.getElementById("cursors").innerHTML = "Cursors: " + cursors + "<br>Cost: " + cursorCost;
+        cursorBtn.innerHTML = "Cursors: " + cursors + "<br>Cost: " + cursorCost;
         updateStats();
     }
 }
 
 // On grandma button click
 function grandmaClick() {
-    var grandmaCost = 100 + 100 * grandmas/2;
     if (cookies >= grandmaCost) {
         cookies -= grandmaCost;
         grandmas++;
 
         grandmaCost = 100 + 100 * grandmas/2;
-        document.getElementById("grandmas").innerHTML = "Grandmas: " + grandmas + "<br>Cost: " + grandmaCost;
+        grandmaBtn.innerHTML = "Grandmas: " + grandmas + "<br>Cost: " + grandmaCost;
         updateStats();
     }
 }
@@ -56,13 +60,13 @@ function handleGrandmas() {
 
 // Update cookies and score
 function updateStats() {
-    document.getElementById("cookies").innerHTML = cookies + " cookies";
-    document.getElementById("score").innerHTML = "Score: " + score;
+    numOfCookiesElm.innerHTML = cookies + " cookies";
+    scoreElm.innerHTML = "Score: " + score;
 }
 
 // Add event listeners for cursor and grandma buttons
-document.getElementById("cursors").addEventListener("click", cursorClick);
-document.getElementById("grandmas").addEventListener("click", grandmaClick);
+cursorBtn.addEventListener("click", cursorClick);
+grandmaBtn.addEventListener("click", grandmaClick);
 
 // Increment cookies based on cursors/grandmas every set time interval
 window.setTimeout(handleCursors, 1000);
